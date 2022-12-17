60cfa412d703698c11b4397410950162f51678e4
Carmen Chow <carmendevelops@gmail.com>
2022-12-17 15:37:27 -0500

    first test

--- test/test-server.js
+++ test/test-server.js
@@ -3,3 +3,13 @@
 var expect = require('chai').expect;
 
 // set up test suites
+
+
+// Sanity check to tsee if mocha's running:
+describe('Mocha', function() {
+  // Test spec (unit test)
+  it('should run our tests using npm', function(){
+    expect(true).to.be.ok;
+
+  })
+});
